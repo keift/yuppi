52f8015752364ec02aa2661230258baacccb3bcb
Fırat Özden <fir4tozden@gmail.com>
2025-06-08 17:57:59 +0300

    v1.0.0

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import * as Atomically from "atomically";
-import * as FSExtra from "fs-extra";
-import * as GracefulFS from "graceful-fs";
-
-import type { Fs } from "./types/main.type";
-
-export const fs: Fs = Object.assign({}, GracefulFS, FSExtra, Atomically);
-
-export type * as Types from "./types/main.type";
--- src/types/Fs.type.ts
+++
@@ -1,5 +0,0 @@
-import type * as Atomically from "atomically";
-import type * as FSExtra from "fs-extra";
-import type * as GracefulFS from "graceful-fs";
-
-export type Fs = typeof Atomically & typeof FSExtra & typeof GracefulFS;
