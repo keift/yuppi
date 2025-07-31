a88e44cc0d4d842666e198cb0d6fe305f976794f
Fırat Özden <fir4tozden@gmail.com>
2025-07-31 06:33:47 +0300

    v1.2.3

--- tests/Patterns/PhoneNumber.test.ts
+++ tests/Patterns/PhoneNumber.test.ts
@@ -2,7 +2,7 @@ import { Patterns } from "../../src/main";
 
 const correct_properties: string[] = ["0090-5555555555"];
 
-const faulty_properties: string[] = ["0090+5555555555", "905555555555", "5555555555"];
+const faulty_properties: string[] = ["0090+5555555555", "90-5555555555", "905555555555", "5555555555"];
 
 for (let i: number = 0; i < correct_properties.length; i++) {
   if (new RegExp(Patterns.PhoneNumber).test(correct_properties[i])) {
@@ -15,3 +15,4 @@ for (let i: number = 0; i < faulty_properties.length; i++) {
     console.log(`✅ Success ${i + 1}/${faulty_properties.length} [FAULTY_PROPERTIES]`);
   } else throw new Error(`❌ Error ${i + 1}/${faulty_properties.length} [FAULTY_PROPERTIES]`);
 }
+
