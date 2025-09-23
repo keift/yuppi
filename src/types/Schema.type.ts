9faf5e0b702e7f7616475f408d70d4a42ae65507
фырат ёздэн <31664778+fir4tozden@users.noreply.github.com>
2025-09-23 03:45:40 +0300

    v1.2.9

--- src/types/Schema.type.ts
+++ src/types/Schema.type.ts
@@ -1,51 +1,63 @@
 /* eslint-disable @typescript-eslint/array-type */
 
-export type Base = {
-  default?: unknown;
-  nullable: boolean;
-  required: boolean;
-};
-
-export type String = Base & {
+export type String = {
   type: 'string';
+  default?: string;
   enum?: string[];
   pattern?: string;
   min?: number;
   max?: number;
   lowercase?: boolean;
   uppercase?: boolean;
+  nullable: boolean;
+  required: boolean;
 };
 
-export type Number = Base & {
+export type Number = {
   type: 'number';
+  default?: number;
   enum?: number[];
   min?: number;
   max?: number;
   integer?: boolean;
   positive?: boolean;
   negative?: boolean;
+  nullable: boolean;
+  required: boolean;
 };
 
-export type Boolean = Base & {
+export type Boolean = {
   type: 'boolean';
+  default?: boolean;
+  nullable: boolean;
+  required: boolean;
 };
 
-export type Date = Base & {
+export type Date = {
   type: 'date';
+  default?: string;
   min?: string;
   max?: string;
+  nullable: boolean;
+  required: boolean;
 };
 
-export type Object = Base & {
+export type Object = {
   type: 'object';
+  default?: Schema;
   properties: Schema;
+  nullable: boolean;
+  required: boolean;
 };
 
-export type Array = Base & {
+export type Array = {
   type: 'array';
+  default?: Types;
   min?: number;
   max?: number;
   items: Types;
+  nullable: boolean;
+  required: boolean;
 };
 
 export type Types = String | Number | Boolean | Date | Object | Array;
