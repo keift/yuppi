227da27757ab48e63c48473d700cd1c74de3af50
fir4tozden <fir4tozden@gmail.com>
2026-01-03 04:18:32 +0300

    v1.3.15

--- tests/Dependencies.test.ts
+++ tests/Dependencies.test.ts
@@ -1,7 +1,5 @@
 import Package from '../package.json';
 
-if (Package.dependencies['@types/node'] !== 'latest') throw new Error('❌ Error: @types/node');
-
 if (Package.devDependencies.neatlint !== 'latest') throw new Error('❌ Error: neatlint');
 if (Package.devDependencies.prettier !== 'latest') throw new Error('❌ Error: prettier');
 if (Package.devDependencies.tsup !== 'latest') throw new Error('❌ Error: tsup');
