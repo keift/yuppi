d3c2addbfc26dbe7dbc83c38091a9b400418af81
fir4tozden <fir4tozden@gmail.com>
2026-04-17 05:29:47 +0300

    v1.5.0

--- tests/Dependencies.test.ts
+++ tests/Dependencies.test.ts
@@ -2,6 +2,6 @@ import Package from '../package.json';
 
 if (Package.devDependencies.prettier !== 'latest') throw new Error('❌ Error: prettier');
 if (Package.devDependencies.rulint !== 'latest') throw new Error('❌ Error: rulint');
-if (Package.devDependencies.tsup !== 'latest') throw new Error('❌ Error: tsup');
+if (Package.devDependencies.tsdown !== 'latest') throw new Error('❌ Error: tsdown');
 
 console.log('✅ Success');
