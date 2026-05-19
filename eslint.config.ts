d3439379d3c6e3f6d11040dd70f16a0233fb5ede
Fırat Özden <fir4tozden@gmail.com>
2026-05-19 23:47:42 +0300

    v1.5.0

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,17 +1,3 @@
 import { rulint } from 'rulint';
 
-export default rulint({
-  ts: {
-    rules: {
-      '@typescript-eslint/naming-convention': [
-        'error',
-        {
-          selector: ['variable', 'classProperty', 'classMethod', 'objectLiteralMethod', 'function'],
-          format: ['snake_case', 'PascalCase'],
-          leadingUnderscore: 'allow',
-          trailingUnderscore: 'forbid'
-        }
-      ]
-    }
-  }
-});
+export default rulint();
