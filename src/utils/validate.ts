61a64fad3a44cfc7fd51a877c95dbb65d4030e06
Fırat Özden <fir4tozden@gmail.com>
2026-06-30 02:35:42 +0300

    v2.0.0

--- src/utils/validate.ts
+++ src/utils/validate.ts
@@ -16,32 +16,26 @@ const format_path_to_string = (path: (string | number)[]) =>
 
 const get_received_type = (value: unknown) => typof(value)[0] as IssueType['received'];
 
-export const validate = <const _Schema extends Schema>(schema: _Schema, data: unknown, options: YuppiOptions) => {
-  const issues: Issue[] = [];
+const report_issue = (type: TypeSingle, issue: Issue, issues: Issue[], options: YuppiOptions) => {
+  const message = (options.issue_messages as Record<string, Record<string, (issue: Issue) => string>>)[type.type][issue.type];
 
-  const report_issue = (type: TypeSingle, issue: Issue) => {
-    const message = (options.issue_messages as Record<string, Record<string, (issue: Issue) => string>>)[type.type][issue.type];
+  if (typeof message === 'function') {
+    issue.message = message(issue);
+  }
 
-    if (typeof message === 'function') {
-      issue.message = message(issue);
-    }
+  issues.push(issue);
 
-    issues.push(issue);
+  if (options.validation?.abort_early === true && issues.length === 1) {
+    throw new ValidationError({ issues });
+  }
+};
 
-    if (options.validation?.abort_early === true && issues.length === 1) {
-      throw new ValidationError({ issues });
-    }
-  };
-
-  const validate_type_single = (type: TypeSingle, data: unknown, path: (string | number)[]) => {
-    let result: unknown;
-    if (Date.now()) {
-      result = { selam: true };
-    }
-
-    if (type.type === 'string') {
-      if (typeof data !== 'string') {
-        report_issue(type, {
+const validate_type_single = (type: TypeSingle, data: unknown, path: (string | number)[], issues: Issue[], options: YuppiOptions) => {
+  if (type.type === 'string') {
+    if (typeof data !== 'string') {
+      report_issue(
+        type,
+        {
           type: 'type',
           expected: type.type,
           received: get_received_type(data),
@@ -49,30 +43,30 @@ export const validate = <const _Schema extends Schema>(schema: _Schema, data: un
           path,
           texts: { path: format_path_to_string(path) },
           message: ''
-        });
-      }
+        },
+        issues,
+        options
+      );
     }
+  }
 
-    return { result, issues };
-  };
+  return data;
+};
 
-  const validate_type_union = (types: TypeUnion, data: unknown, path: (string | number)[]) => {
-    for (const type of types) {
-      const result = validate_type_single(type, data, path);
+const validate_type_union = (types: TypeUnion, data: unknown, path: (string | number)[], options: YuppiOptions) => {};
 
-      if (result.issues.length === 0) {
-        return result;
-      }
-    }
-  };
+const validate_type = (type: Type, data: unknown, path: (string | number)[], options: YuppiOptions) => {};
 
-  const validate_type = (type: Type, data: unknown, path: (string | number)[]) => {
-    if (Array.isArray(type)) {
-      return validate_type_union(type, data, path);
-    } else {
-      return validate_type_single(type, data, path);
-    }
-  };
+const validate_schema_single = (schema: SchemaSingle, data: unknown, path: (string | number)[], options: YuppiOptions) => {};
 
-  return validate_type_single(schema, data, []) as InferSchema<_Schema>;
+const validate_schema_union = (schemas: SchemaUnion, data: unknown, path: (string | number)[], options: YuppiOptions) => {};
+
+const validate_schema = (schema: Schema, data: unknown, path: (string | number)[], options: YuppiOptions) => {};
+
+export const validate = (schema: Schema, data: unknown, options: YuppiOptions) => {
+  if (typeof data === 'object') {
+    data = structuredClone(data);
+  }
+
+  return data;
 };
